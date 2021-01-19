cd08d02e2fa44dcd443fd7784967326933fad1e3
Michael Morrison <morri909@umn.edu>
2021-01-18 19:02:05 -0600

    Refactor services to common interface, closes #11

--- pet-clinic-data/src/main/java/com/spydrone/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/spydrone/sfgpetclinic/services/PetService.java
@@ -2,11 +2,5 @@ package com.spydrone.sfgpetclinic.services;
 
 import com.spydrone.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-	Pet findById(Long id);
-	Pet save(Pet pet);
-	Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
--- pet-clinic-data/src/main/java/com/spydrone/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/spydrone/sfgpetclinic/services/VetService.java
@@ -2,11 +2,5 @@ package com.spydrone.sfgpetclinic.services;
 
 import com.spydrone.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-	Vet findByID(Long id);
-	Vet save(Vet vet);
-	Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
