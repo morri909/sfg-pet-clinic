02e7155faa87b19aba3481f622fb6fb26ea9255d
Michael Morrison <morri909@umn.edu>
2021-01-19 18:31:43 -0600

    Implement vet controller and index page, closes #7

---
+++ pet-clinic-web/src/main/java/com/spydrone/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package com.spydrone.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+	@RequestMapping({"/vets", "vets/index", "/vets/index.html"})
+	public String list() {
+		return "vets/index";
+	}
+}
