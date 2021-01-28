861d0449ec0f9d848cd46bd6628a3548f1ac495a
Michael Morrison <morri909@umn.edu>
2021-01-27 18:25:57 -0600

    Create visit JPA entity, add link to pet, closes #38

--- pet-clinic-data/src/main/java/com/spydrone/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/spydrone/sfgpetclinic/model/Pet.java
@@ -2,6 +2,7 @@ package com.spydrone.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -17,6 +18,8 @@ public class Pet extends BaseEntity {
 	private Owner owner;
 	@Column(name = "birth_date")
 	private LocalDate birthDate;
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+	private Set<Visit> visits;
 
 	public String getName() {
 		return name;
@@ -48,5 +51,13 @@ public class Pet extends BaseEntity {
 
 	public void setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
+	}
+
+	public Set<Visit> getVisits() {
+		return visits;
+	}
+
+	public void setVisits(Set<Visit> visits) {
+		this.visits = visits;
 	}
 }
