6598d7a2006ab50e04a501d4fd986a91e14b097e
Michael Morrison <morri909@umn.edu>
2021-02-22 18:37:23 -0600

    Implement vets json endpoint

--- pet-clinic-web/src/main/java/com/spydrone/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/spydrone/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.spydrone.sfgpetclinic.controllers;
 
+import com.spydrone.sfgpetclinic.model.Vet;
 import com.spydrone.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -14,9 +19,15 @@ public class VetController {
 		this.vetService = vetService;
 	}
 
-	@RequestMapping({"/vets", "vets/index", "/vets/index.html", "/vets.html"})
+	@GetMapping({"/vets", "vets/index", "/vets/index.html", "/vets.html"})
 	public String list(Model model) {
 		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
+	}
+
+	@GetMapping("/api/vets")
+	@ResponseBody
+	public Set<Vet> getVets() {
+		return vetService.findAll();
 	}
 }
